895c1765f36c55c33f3a53151516f19adda3b331
bo <cbs1006@naver.com>
2024-11-29 16:46:50 +0900

    Feat: 회원 휴면 스케줄러 추가

--- batch/src/main/java/shop/s5g/batch/repository/customer/MemberRepository.java
+++ batch/src/main/java/shop/s5g/batch/repository/customer/MemberRepository.java
@@ -1,9 +1,12 @@
 package shop.s5g.batch.repository.customer;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.s5g.batch.entity.customer.Member;
+import shop.s5g.batch.entity.customer.MemberStatus;
 import shop.s5g.batch.repository.customer.qdsl.MemberQuerydslRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberQuerydslRepository {
-
+    List<Member> findByLatestLoginAtBeforeAndStatus(LocalDateTime latestLoginAt, MemberStatus status);
 }
